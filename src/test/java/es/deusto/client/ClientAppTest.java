7bd06af05d450a48a16f192aab3e0109f06e1d91
Jon Díaz <jondi@opendeusto.es>
2021-04-29 08:47:24 +0200

    Added ClientAppTest.java

---
+++ src/test/java/es/deusto/client/ClientAppTest.java
@@ -0,0 +1,13 @@
+package es.deusto.client;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class ClientAppTest {
+
+	@Test
+	public void prueba() {
+		assertEquals(6,6);
+	}
+	
+}
