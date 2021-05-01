51488f7ecd19b631c8e848ef82a6cd974e1067e2
Jon Díaz <jondi@opendeusto.es>
2021-05-01 21:12:27 +0200

    JUnit tests for the client added

--- src/test/java/es/deusto/client/ClientAppTest.java
+++ src/test/java/es/deusto/client/ClientAppTest.java
@@ -1,13 +1,37 @@
 package es.deusto.client;
 
 import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
+import es.deusto.client.windows.EmployeeInfoWindow;
+import es.deusto.serialization.EmployeeData;
+
 public class ClientAppTest {
+	
+	EmployeeInfoWindow employeeWindow; 
+	EmployeeData employee;
+	
+	@Before
+	public void init() {
+		employee = new EmployeeData();
+		employee.setId(0);
+		employee.setName("test");
+		employee.setAddress("testAddres");
+		employee.setDepartment("testDepart");
+		
+		employeeWindow = new EmployeeInfoWindow("test2", employee);
+
+	}
 
 	@Test
 	public void prueba() {
-		assertEquals(6,6);
+		assertEquals(Integer.parseInt(employeeWindow.gettId().getText()), employee.getId());
+		assertEquals(employeeWindow.gettName().getText(), employee.getName());
+		assertEquals(employeeWindow.gettAddress().getText(), employee.getAddress());
+		assertEquals(employeeWindow.gettDepartment().getText(), employee.getDepartment());
+
 	}
 	
 }
--- src/test/java/es/deusto/testing/junit/ContiperfSimple.java
+++ src/test/java/es/deusto/testing/junit/ContiperfSimple.java
@@ -7,10 +7,11 @@ import org.databene.contiperf.junit.ContiPerfRule;
 public class ContiperfSimple {
  @Rule
  public ContiPerfRule i = new ContiPerfRule();
+ 
  @Test
  @PerfTest(invocations = 1000, threads = 20)
  @Required(max = 1200, average = 250)
  public void test1() throws Exception {
- Thread.sleep(200);
+	 Thread.sleep(200);
  }
 }
