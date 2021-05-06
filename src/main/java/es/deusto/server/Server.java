70a5d98eab2aba8a3463c220e5d7b5b4584cb636
Jon Díaz <jondi@opendeusto.es>
2021-05-06 21:56:42 +0200

    Server structure updated

--- src/main/java/es/deusto/server/Server.java
+++ src/main/java/es/deusto/server/Server.java
@@ -1,5 +1,7 @@
 package es.deusto.server;
 
+import java.util.ArrayList; 
+import java.util.List;
 import java.util.Locale;
 import java.util.ResourceBundle;
 import java.util.logging.Logger;
@@ -11,6 +13,7 @@ import javax.jdo.JDOHelper;
 import javax.jdo.PersistenceManager;
 import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Transaction;
+import javax.jdo.Query;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -39,9 +42,6 @@ public class Server {
 	@POST
 	@Path("/addEmployee")
 	public Response addEmployee(EmployeeData employeeData) {
-		log.info("*****************");
-		
-		System.out.println(employeeData.toString());
 		try {	
             tx.begin();
             log.info(resourceBundle.getString("add_employee"));
@@ -68,22 +68,58 @@ public class Server {
 	@POST
 	@Path("/updateEmployee")
 	public Response updateEmployee(EmployeeData employeeData) {
-		ServerManager.manager.updateEmployee(employeeData);
+		try {	
+            tx.begin();
+            log.info(resourceBundle.getString("update_employee"));
+			Employee employee = null;
+			try {
+				employee = pm.getObjectById(Employee.class, employeeData.getId());
+				pm.retrieve(employee);
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				log.info(resourceBundle.getString("err_empl_not_found"));
+			}
+			employee.setId(employeeData.getId());
+			employee.setName(employeeData.getName());
+			employee.setAddress(employeeData.getAddress());
+			employee.setDepartment(employeeData.getDepartment());
+			pm.makePersistent(employee);
+			tx.commit();
+			log.info(resourceBundle.getString("empl_upd_correct"));
+        } finally {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+		} 
 		return Response.ok().build();
 	}
 	
 	@POST
 	@Path("/deleteEmployee")
 	public Response deleteEmployee(int id) {
-		ServerManager.manager.deleteEmployee(id);
-		return Response.ok().build();
+		try {	
+            tx.begin();
+            log.info(resourceBundle.getString("deleting_employee"));
+			Employee employee = null;
+			try {
+	            log.info(resourceBundle.getString("getting_employee"));
+				employee = pm.getObjectById(Employee.class, id);
+				pm.deletePersistent(employee);
+			} catch (javax.jdo.JDOObjectNotFoundException jonfe) {
+				log.info(resourceBundle.getString("err_empl_not_found"));
+			}
+			tx.commit();
+            log.info(resourceBundle.getString("ok_del_empl"));
+        } finally {
+            if (tx.isActive()) {
+                tx.rollback();
+            }
+		}		return Response.ok().build();
 	}
 
 	@GET
 	@Path("/getEmployees")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getEmployees() {
-		/**
 		try
 		{	 
 	        tx.begin();
@@ -111,7 +147,7 @@ public class Server {
             {
                 tx.rollback();
             }
-		}**/
+		}
 		return Response.ok().build();
 	}
 	
