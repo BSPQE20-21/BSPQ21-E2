389c101ce6f2ecd3d6962de602a8d547f28f8733
Irati98o <irati98o@192.168.1.52>
2021-05-22 19:34:50 +0200

    Employee Info Window test

---
+++ src/test/java/es/deusto/client/windows/EmployeeInfoWindowTest.java
@@ -0,0 +1,43 @@
+package es.deusto.client.windows;
+
+import static org.junit.Assert.*;
+
+import java.awt.HeadlessException;
+
+import org.databene.contiperf.PerfTest;
+import org.databene.contiperf.Required;
+import org.junit.Before;
+import org.junit.Test;
+
+import es.deusto.client.ClientApp;
+import es.deusto.serialization.EmployeeData;
+
+public class EmployeeInfoWindowTest {
+	EmployeeInfoWindow eiw;
+	ClientApp ca;
+	EmployeeData expected;
+	ClientWindow cw;
+
+	@Before
+	public void setUp() {
+		try {
+			
+			ca = new ClientApp("127.0.0.1", "8080");
+			cw = new ClientWindow(ca);
+			expected = new EmployeeData(983473, "Gorka", "testAdress", "testDepart");
+			
+		} catch(HeadlessException e) {
+			
+		}
+	}
+	
+	
+	@Test
+	@PerfTest(invocations = 100, threads = 2)
+	@Required(max = 200, average = 100)
+	public void testEmployeeInfoWindow() {
+		eiw = new EmployeeInfoWindow("Employee Info Window test");
+		assertTrue(eiw.isVisible());
+	}
+
+}
