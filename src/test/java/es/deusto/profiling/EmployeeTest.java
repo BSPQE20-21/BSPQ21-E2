fc06fa2b2b62d6ed6366060d4092807dbc507a51
Irati98o <irati98o@localhost>
2021-05-23 12:08:15 +0200

    Created Employee Test

---
+++ src/test/java/es/deusto/profiling/EmployeeTest.java
@@ -0,0 +1,40 @@
+package es.deusto.profiling;
+
+import static org.junit.Assert.*;
+
+import java.awt.HeadlessException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class EmployeeTest {
+	
+	private Path path;
+	
+	@Before
+	public void init() {
+		try {
+			this.path = Paths.get("x:\\path\\Employee");
+		} 
+		catch(HeadlessException e) {
+		
+		} 
+	}
+
+	@Test
+	public void testReadListFromFolder() throws IOException{
+		try {
+			Path file = this.path.resolve("testFolder");
+			boolean content = Files.isReadable(file);
+			assertTrue(content);
+		} catch (HeadlessException e){
+		
+		} 
+	}
+
+}
