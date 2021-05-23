5a6619ade74e98ee42b6f579c73c351f200f2c51
Irati98o <irati98o@localhost>
2021-05-23 12:22:48 +0200

    Get content test

---
+++ src/test/java/es/deusto/profiling/EmployeeProfilingTest.java
@@ -0,0 +1,23 @@
+package es.deusto.profiling;
+
+import static org.junit.Assert.*;
+
+import java.awt.HeadlessException;
+
+import org.junit.Test;
+
+public class EmployeeProfilingTest {
+	String content = "testContent";
+
+	@Test
+	public void testGetContent() {
+		try {
+			EmployeeProfiling ep = new EmployeeProfiling();
+			assertEquals(content, ep.getContent());
+			
+		} catch(HeadlessException e) {
+			
+		}
+	}
+
+}
